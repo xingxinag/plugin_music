b1f6d58421f5e0bb504d176c82134be60965c0c5
小星虎 <105782492+xingxinag@users.noreply.github.com>
2025-01-02 19:24:20 +0800

    更新 __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,5 @@
 from .qq_music import QQMusicService
 from .netease_music import NeteaseMusicService
 from .kugou_music import KugouMusicService
+
+__all__ = ["QQMusicService", "NeteaseMusicService", "KugouMusicService"]
